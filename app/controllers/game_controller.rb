5ed1225ccd8ab7566966449af3872b66ac94779e
aryan <aryan.singh.lamba@gmail.com>
2017-07-10 22:08:26 -0700

    fixed login bug withh best scores

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,18 +4,22 @@ class GameController < ApplicationController
   end
 
   def save_score
-    score = params[:score].to_i
-    best_score = BestScore.where("user_id = ?", current_user.id).first
-    if best_score == nil
-      best_score = BestScore.new
-      best_score.user = current_user
-      best_score.score = 0
+    if current_user == nil
+      render :json => {:message => 'Login to save your score'}
+    else
+      score = params[:score].to_i
+      best_score = BestScore.where("user_id = ?", current_user.id).first
+      if best_score == nil
+        best_score = BestScore.new
+        best_score.user = current_user
+        best_score.score = 0
+      end
+      if score > best_score.score
+        best_score.score = score
+        best_score.save
+      end
+      render :json => {:message => best_score.score.to_s + ' seconds best'}
     end
-    if score > best_score.score
-      best_score.score = score
-      best_score.save
-    end
-    render :json => best_score.score.to_s
   end
 
   def get_score
